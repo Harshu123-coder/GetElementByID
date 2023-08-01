3fa61ece3788178cb447287046eeb08248aa8c27
Harshu123-coder <harshuamule@gmail.com>
2023-08-01 09:00:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var header = document.getElementById('header-title');
+console.log(header);
+
+header.innerHTML="hello";
+header.innerText="Bye";
+header.textContent="Harsha";
+
+header.style.backgroundColor="red";
